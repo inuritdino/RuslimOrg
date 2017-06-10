2071a6d15f7b7daf7abde207c3b57e106f6cd77d
Ilya Potapov <elias.potapov@gmail.com>
2017-06-10 21:08:45 +0300

    Automated commit at 2017-06-10 18:08:45 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,10 +1,14 @@
 //= require lunr.min
+
+var lunrIndex = null;
+var lunrData  = null;
+
 function hideRightPanel() {
     var x = document.getElementById("right").style.display;
     if(x != "none"){
 	document.getElementById("hide-right").innerHTML = "&laquo;";
 	document.getElementById("right").style.display = "none";
-	document.getElementById("content").style.width = "calc(100% - 40px)";
+	document.getElementById("content").style.width = "1006px";
 	document.getElementById("content").style["border-style"] = "none";
     }
     if(x === "none"){
@@ -21,9 +25,6 @@ function hideRightPanel() {
     }
 }
 
-var lunrIndex = null;
-var lunrData  = null;
-
 // Download index data
 $.ajax({
   url: '/lunr-index.json',
@@ -35,17 +36,18 @@ $.ajax({
   }
 });
 
-function showSearchResults(search_term){
-    var = lunrIndex.search(search_term);
-    var result = _(lunrIndex.search(request.term)).take(50).pluck('ref').map(function(ref) {
-        return lunrData.docs[ref];
-    }).value();
-    if (result.length == 0){
-	//document.getElementById("content").innerHTML = "No results found";
-	document.write("No results found");
-    }
-    else {
-	//document.getElementById("content").innerHTML = result[0];
-	document.write("Something was found!");
-    }
-}
+// function showSearchResults(searchTerm){
+//     document.getElementById("search-box").innerHTML = searchTerm;
+//     var = lunrIndex.search(searchTerm);
+//     var result = _(lunrIndex.search(request.term)).take(50).pluck('ref').map(function(ref) {
+//         return lunrData.docs[ref];
+//     }).value();
+//     if (result.length == 0){
+// 	//document.getElementById("content").innerHTML = "No results found";
+// 	document.write("No results found");
+//     }
+//     else {
+// 	//document.getElementById("content").innerHTML = result[0];
+// 	document.write("Something was found!");
+//     }
+// }
